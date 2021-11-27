7d41cb81df33ec66f1d215247c17d3b1c483bc54
Avi Gupta <41137189+avigupta10@users.noreply.github.com>
2021-11-27 14:50:30 +0530

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,17 +1,34 @@
-from .models import *
 from dateutil import parser
+from django.conf import settings
+from django.core.mail import send_mail
+from django.db.models import Q
+
+from .models import *
 
 
 def collide(can_id, int_id, cur_start, cur_end):
-    can = Candidate.objects.get(id=can_id)
-    int = Candidate.objects.get(id=int_id)
-    cur_start = parser.parse(cur_start)
-    cur_end = parser.parse(cur_end)
+    candidate = Candidate.objects.get(id=can_id)
+    interviewer = Candidate.objects.get(id=int_id)
 
-    Inte = Interview.objects.filter(candidate_name__name=can.name, interviewer_name__name=int.name)
-    if Inte.exists():
-        Inte = Inte[0]
-    if (Inte.start_time < cur_start.time() < Inte.end_time) or (Inte.start_time < cur_end.time() < Inte.end_time) or (
-            cur_start.time() <= Inte.start_time and cur_end.time() >= Inte.end_time):
-        return False
-    return True
+    cur_start = parser.parse(cur_start).time()
+    cur_end = parser.parse(cur_end).time()
+
+    time_filter = Q(start_time__range=[cur_start, cur_end]) | Q(
+        end_time__range=[cur_start, cur_end])
+
+    candidate_objs = Interview.objects.filter(candidate_name__name=candidate.name).filter(time_filter)
+    interviewer_objs = Interview.objects.filter(interviewer_name__name=interviewer.name).filter(time_filter)
+
+    objs = candidate_objs | interviewer_objs
+
+    if objs.exists():
+        return True
+    return False
+
+
+def send_confirmation(candidate_name, interviewer_name, start, end, date, recipient_list):
+    subject = f'{candidate_name} X {interviewer_name}'
+    message = f'Hi {candidate_name}, Your interview is scheduled from {start} to {end} on {date}.'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = recipient_list
+    send_mail(subject, message, email_from, recipient_list)
